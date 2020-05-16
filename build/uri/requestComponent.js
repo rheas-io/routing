59d3d7b8d9b6a59e19df6653ba7cd9d3b90ad160
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-16 17:01:49 +0530

    new request uri component

---
+++ build/uri/requestComponent.js
@@ -0,0 +1,54 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var baseComponent_1 = require("./baseComponent");
+var routeParamComponent_1 = require("./routeParamComponent");
+var RequestComponent = /** @class */ (function (_super) {
+    __extends(RequestComponent, _super);
+    function RequestComponent() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    /**
+     * @inheritdoc
+     *
+     * @param status
+     */
+    RequestComponent.prototype.setComponent = function (component) {
+        this._routeComponent = component;
+        return this;
+    };
+    /**
+     * @inheritdoc
+     *
+     * @returns boolean
+     */
+    RequestComponent.prototype.isParam = function () {
+        return this._routeComponent instanceof routeParamComponent_1.ParamComponent;
+    };
+    /**
+     * @inheritdoc
+     *
+     * @return object
+     */
+    RequestComponent.prototype.getParam = function () {
+        var param = {};
+        if (this._routeComponent instanceof routeParamComponent_1.ParamComponent) {
+            param[this._routeComponent.getName()] = this.component;
+        }
+        return param;
+    };
+    return RequestComponent;
+}(baseComponent_1.UriComponent));
+exports.RequestComponent = RequestComponent;
---
+++ src/uri/requestComponent.ts
@@ -0,0 +1,48 @@
+import { UriComponent } from "./baseComponent";
+import { StringObject } from "@rheas/contracts";
+import { ParamComponent } from "./routeParamComponent";
+import { IRequestComponent, IUriComponent } from "@rheas/contracts/routes/uri";
+
+export class RequestComponent extends UriComponent implements IRequestComponent {
+
+    /**
+     * The matching route uri component of this uri component.
+     * 
+     * @var IUriComponent
+     */
+    private _routeComponent: IUriComponent | undefined;
+
+    /**
+     * @inheritdoc
+     * 
+     * @param status 
+     */
+    public setComponent(component: IUriComponent): IRequestComponent {
+        this._routeComponent = component;
+
+        return this;
+    }
+
+    /**
+     * @inheritdoc
+     * 
+     * @returns boolean
+     */
+    public isParam(): boolean {
+        return this._routeComponent instanceof ParamComponent;
+    }
+
+    /**
+     * @inheritdoc
+     * 
+     * @return object
+     */
+    public getParam(): StringObject {
+        const param: StringObject = {};
+
+        if (this._routeComponent instanceof ParamComponent) {
+            param[this._routeComponent.getName()] = this.component;
+        }
+        return param;
+    }
+}
