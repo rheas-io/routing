4041a69c63555b228ba760845b93575842c1c6f9
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-06-08 23:42:25 +0530

    fixed component 100% coverage

--- tests/components.spec.ts
+++ tests/components.spec.ts
@@ -3,15 +3,27 @@ import { FixedComponent } from "../src/uri/routeFixedComponent";
 import { ComponentFactory } from "../src/uri/uriComponentFactory";
 
 describe("Route uri component tests", () => {
-    const uriComponents = ComponentFactory.createFromRoute(new Route('/api/list/:id/subscriber/:id?'));
+    const uriComponents = ComponentFactory.createFromRoute(new Route('/api/list/:id/subscriberså/:id?'));
 
     it("component match test", () => {
         expect(uriComponents[0].equals(new FixedComponent("api"))).toBe(true);
-        expect(uriComponents[0].equals(new FixedComponent(":api"))).toBe(false);
         expect(uriComponents[4].equals(new FixedComponent(":id?"))).toBe(true);
     });
 
+    it("encoded match test", () => {        
+        expect(uriComponents[3].equals(new FixedComponent("subscriberså"))).toBe(true);
+        expect(uriComponents[3].equals(new FixedComponent('subscribers%C3%A5'))).toBe(true);
+        expect(uriComponents[3].equals(new FixedComponent('subscribersåƒ'))).toBe(false);
+        expect(uriComponents[3].equals(new FixedComponent('subscribers%C3%A5%C6%92'))).toBe(false);
+    });
+
     it("invalid input test", () => {
+        //@ts-ignore
+        expect(uriComponents[0].equals(null)).toBe(false);
+        //@ts-ignore
+        expect(uriComponents[0].equals(undefined)).toBe(false);
+        expect(uriComponents[0].equals(new FixedComponent(":api"))).toBe(false);
+
         let toCheck = new FixedComponent(" ");
         expect(new FixedComponent("api").equals(toCheck)).toBe(false);
 
