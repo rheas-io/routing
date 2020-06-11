42f585702c21c713b2919e13f7c6904b0889cfc7
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-06-11 16:55:10 +0530

    methods check added

--- tests/route.spec.ts
+++ tests/route.spec.ts
@@ -49,7 +49,28 @@ const namedRoutes: KeyValue<IRoute> = [
     return routes;
 }, {});
 
-describe("route class", () => {
+describe("route", () => {
+
+    // Route methods check
+    it("methods check", () => {
+        let route = Route.all("/api", "");
+        expect(route.getMethods()).toEqual(Route.verbs);
+
+        route = Route.get('/', '');
+        expect(route.getMethods()).toContain("GET");
+        expect(route.getMethods()).toContain("HEAD");
+        expect(route.getMethods()).not.toContain("POST");
+
+        route = Route.put('/user', '');
+        expect(route.getMethods()).toContain("PUT");
+        expect(route.getMethods()).not.toContain("HEAD");
+
+        route = new Route().methods("GET");
+        expect(route.getMethods()).toContain("HEAD");
+
+        // Throw error when invalid method is used
+        expect(() => { route = new Route().methods("ANY"); }).toThrow();
+    });
 
     //Test route path string
     it("path_test", () => {
