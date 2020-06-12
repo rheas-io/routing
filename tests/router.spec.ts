b578b41fc924a943c2924018688f0f4df1fb6da3
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-06-12 23:51:02 +0530

    router test init-validators check

---
+++ tests/router.spec.ts
@@ -0,0 +1,49 @@
+import { Router } from "../src";
+import { MockApp } from "@rheas/mockers";
+import { HostValidator } from "../src/validators/hostValidator";
+import { UriValidator } from "../src/validators/uriValidator";
+import { SchemeValidator } from "../src/validators/schemeValidator";
+import { MethodValidator } from "../src/validators/methodValidator";
+import { blogRoutes, homeRoutes, apiRoutes, faqRoute, pricingRoute, namedRoutes } from "./testRoutes";
+
+class ExtendedRouter extends Router {
+    /**
+     * Asserts all the validators are returning proper types.
+     */
+    public assertValidators() {
+        expect(this.routeValidators()).toEqual(expect.arrayContaining([
+            new HostValidator, new SchemeValidator, new MethodValidator, new UriValidator
+        ]));
+        // Cache check for branch coverage
+        expect(this.routeValidators().length).toBe(4);
+    }
+}
+
+describe("router", () => {
+
+    const router = new ExtendedRouter(new MockApp(__dirname));
+    router.middleware(["trimString", "maintenance"]);
+    router.routes(apiRoutes, homeRoutes);
+    router.cacheRoutes();
+
+    //Route validators
+    it("route_validators", () => { router.assertValidators(); });
+
+    //Route middlewares check. router.routeMiddleware should always
+    // return an empty array and middlewaresToResolve should return the
+    // global middlewares.
+    it("route_middlewares", () => {
+        expect(router.routeMiddlewares()).toEqual([]);
+        expect(router.middlewaresToResolve()).toEqual(expect.arrayContaining([
+            "trimString", "maintenance"
+        ]))
+    });
+
+    // Named routes check
+    it("namedRoutes", () => {
+        expect(router.getNamedRoute('faq')).toBe(faqRoute);
+        expect(router.getNamedRoute('pricing')).toBe(pricingRoute);
+
+        expect(router.getNamedRoute('api')).toBeNull();
+    });
+});
